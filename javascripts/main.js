1a2f5eab4a75e404e86a555eb04b0c45cd94c9a8
Joshua Meyers <joshua.r.meyers@gmail.com>
2017-07-11 10:25:27 -0500

    firebase data to show in console

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
+
+
+// const dataFactory = object.create(null);
+
+
+$.ajax({
+    url: "https://wookiesoftheyear-36e4a.firebaseio.com/.json"
+}).done( (data) => {
+	console.log("firebase", data);
+}).fail( (error) => {
+	console.log("error");
+});
+
+
+
 console.log("yo");
