f59f48d39d8e3213915852d4a7fb65fe6ac8bfac
Glen Vollmer <glenvollmer@Glens-Macbook-Pro.local>
2017-07-13 15:11:30 -0500

    have mvp for time selector

--- javascripts/time.js
+++ javascripts/time.js
@@ -1,10 +1,7 @@
 'use strict';
 
-
 let timeSort = function(parkObj, areaObj, typesObj, attractionObj){
 
-
-	
 	let Handlebars = require('hbsfy/runtime');	
 	let sideBarTemplate = require('../templates/sideBar.hbs');
 	let altTimeTemplate = require('../templates/timeSideBar.hbs');
@@ -14,32 +11,26 @@ let timeSort = function(parkObj, areaObj, typesObj, attractionObj){
 timeSearch.addEventListener("click",function() {
 	let chosenTime = "";
 	chosenTime = event.target.innerHTML;
-	let time = chosenTime.split(':');
+	let time = chosenTime.split(' ');
 	let finalTime = time[0];
-	console.log(finalTime);
 		
 	for (let k = 0; k < attractionObj.length; k++) {
-		// console.log("for loop");
-		if (attractionObj[k].hasOwnProperty("times")!=true) {
-			console.log(attractionObj[k]);
-			$("#ridesToDom").append(altTimeTemplate(attractionObj[k]));
-		} else {}
-		
 
-		// if(!!attractionObj[k].hasOwnProperty("times"));
-		// 	$("#ridesToDom").append(altTimeTemplate(attractionObj[i]));
-		// } else if(timeSearch.value === attractionObj[k].times);
-		// 	$("#ridesToDom").append(sideBarTemplate(attractionObj[i]));	
+		if (attractionObj[k].times) {
+			
+			// for (let x = 0; x < attractionObj[k].times.length; x++) {
+				
+
+				if (attractionObj[k].times.includes(finalTime)) {
+					
+					$("#ridesToDom").append(sideBarTemplate(attractionObj[k]));
+				}
+			// } 
+		// } else {
+				// $("#ridesToDom").append(altTimeTemplate(attractionObj[k]));
+		}	
 	}
 });
 };
 
-
-
 module.exports = {timeSort};
-
-
-// timeSearch.addEventListener("click", function() {
-// 	}
-// }
-// })};
