5240b8b2bf39aa7210a45c052253bfc0fe765ee5
jevonthomas <jevon.g.thomas@gmail.com>
2017-07-14 09:40:39 -0500

    I added a few lines of code to both the name.js and time.js file that will remove the highlighted border from a previously selected grid.

--- javascripts/name.js
+++ javascripts/name.js
@@ -10,20 +10,24 @@ let listRideNames = function(parkObj, areaObj, typesObj, attractionObj){
 	
 	let searchRides = document.getElementById("search-bar");
 	let submitSearch = document.getElementById("submit-button");
+	let eachGrid = document.getElementsByClassName("grid");
 
 submitSearch.addEventListener("click", function() {
-$("#ridesToDom").empty();
-	for (let i = 0; i < attractionObj.length; i++) {
-		if (attractionObj[i].name.toUpperCase().includes(searchRides.value.toUpperCase())) {
-			if (attractionObj[i].hasOwnProperty("times")) {
-				$("#ridesToDom").append(sideBarTemplate(attractionObj[i]));
-			} else {
-				$("#ridesToDom").append(altTimeTemplate(attractionObj[i]));
+	for (let i = 0; i < eachGrid.length; i++) {
+		eachGrid[i].removeAttribute("style");
+	}
+	$("#ridesToDom").empty();
+		for (let i = 0; i < attractionObj.length; i++) {
+			if (attractionObj[i].name.toUpperCase().includes(searchRides.value.toUpperCase())) {
+				if (attractionObj[i].hasOwnProperty("times")) {
+					$("#ridesToDom").append(sideBarTemplate(attractionObj[i]));
+				} else {
+					$("#ridesToDom").append(altTimeTemplate(attractionObj[i]));
+				}
 			}
 		}
-	}
-	
-});	
+		
+	});	
 };
 
 module.exports = {listRideNames};
--- javascripts/time.js
+++ javascripts/time.js
@@ -7,22 +7,27 @@ let timeSort = function(parkObj, areaObj, typesObj, attractionObj){
 	let altTimeTemplate = require('../templates/timeSideBar.hbs');
 	let timeSearch = document.getElementById("time-search");
 
-timeSearch.addEventListener("click", function() {
-    $("#ridesToDom").empty();
-	let chosenTime = event.target.getAttribute('value');
-    console.log("time", chosenTime);	
-	for (let i = 0; i < attractionObj.length; i++) {
-        if (attractionObj[i].times) {
-			// for (let x = 0; x < attractionObj[k].times.length; x++) {
-			if (attractionObj[i].times.toString().includes(chosenTime)) {
-                // console.log("attrObj", attractionObj[i].times.toString());
-				$("#ridesToDom").append(sideBarTemplate(attractionObj[i]));
-		    }  else {
-				 $("#ridesToDom").append(altTimeTemplate(attractionObj[i]));
-            }
-        }	
-	}
-});
+	let eachGrid = document.getElementsByClassName("grid");
+
+	timeSearch.addEventListener("click", function() {
+		for (let i = 0; i < eachGrid.length; i++) {
+			eachGrid[i].removeAttribute("style");
+		}
+	  $("#ridesToDom").empty();
+		let chosenTime = event.target.getAttribute('value');
+	  console.log("time", chosenTime);	
+		for (let i = 0; i < attractionObj.length; i++) {
+	    if (attractionObj[i].times) {
+				// for (let x = 0; x < attractionObj[k].times.length; x++) {
+				if (attractionObj[i].times.toString().includes(chosenTime)) {
+	        // console.log("attrObj", attractionObj[i].times.toString());
+					$("#ridesToDom").append(sideBarTemplate(attractionObj[i]));
+			    }  else {
+					 $("#ridesToDom").append(altTimeTemplate(attractionObj[i]));
+        }
+      }
+		}
+	});
 };
 
 module.exports = {timeSort};
