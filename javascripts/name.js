219b34a624ab15af8ddf67f6dd0b9b1b589c6fbd
Dustin Walker <thewalkman1234@gmail.com>
2017-07-14 11:24:02 -0500

    type id stuff

--- javascripts/name.js
+++ javascripts/name.js
@@ -2,26 +2,54 @@
 
 let listRideNames = function(parkObj, areaObj, typesObj, attractionObj){
 
-	console.log("stuff", attractionObj);
+	// console.log("stuff", attractionObj);
 	
 	let Handlebars = require('hbsfy/runtime');	
 	let sideBarTemplate = require('../templates/sideBar.hbs');
 	let altTimeTemplate = require('../templates/timeSideBar.hbs');
+	// let typesTemplate = require('../templates/partials/types.hbs');
+	// Handlebars.registerPartial("h4", require('../templates/partials/types.hbs'));
 	
 	let searchRides = document.getElementById("search-bar");
 	let submitSearch = document.getElementById("submit-button");
 
+	let typeID = "";
+	console.log("please work", typeID);
+
 submitSearch.addEventListener("click", function() {
 $("#ridesToDom").empty();
 	for (let i = 0; i < attractionObj.length; i++) {
+		typeID = attractionObj[i].type_id;
+		// for (let q = 0; q < typesObj.length; q++) {
+		// 	if(typeID === typesObj[q].id) {
+		// 		typeID = typesObj[q].name;
+		// 		console.log("maybe", typeID);
+				// $("#metaHandleBars").append(typesTemplate(typesObj[q].name));
+				// console.log("Hello!?!", typesObj[q].name);
+			
 		if (attractionObj[i].name.toUpperCase().includes(searchRides.value.toUpperCase())) {
-			if (attractionObj[i].hasOwnProperty("times")) {
-				$("#ridesToDom").append(sideBarTemplate(attractionObj[i]));
-			} else {
-				$("#ridesToDom").append(altTimeTemplate(attractionObj[i]));
-			}
+			for (let q = 0; q < typesObj.length; q++) {
+			if(typeID === typesObj[q].id) {
+				typeID = typesObj[q].name;
+				if (attractionObj[i].hasOwnProperty("times")) {
+					let tempObj = {};
+					tempObj.name = attractionObj[i].name;
+					tempObj.id = typeID;
+					tempObj.description = attractionObj[i].description;
+					tempObj.times = attractionObj[i].times;
+					console.log("tempObj", tempObj);
+					$("#ridesToDom").append(sideBarTemplate(tempObj));
+				} else {
+					let tempObj = {};
+					tempObj.name = attractionObj[i].name;
+					tempObj.id = typeID;
+					tempObj.description = attractionObj[i].description;
+					tempObj.times = attractionObj[i].times;
+					console.log("tempObj", tempObj);
+					$("#ridesToDom").append(altTimeTemplate(tempObj));
+				}
 		}
-	}
+	}}}
 	
 });	
 };
